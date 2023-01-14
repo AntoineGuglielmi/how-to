6270d5dfa91246ea0adb6f913dbe406113106448
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-01-14 14:51:24 +0100

    ok

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-howToPurple-900 min-h-screen flow-root font-nunito text-white pb-[5rem]">
+  <div class="bg-howToPurple-900 min-h-screen flow-root font-nunito text-white pb-[5rem] overflow-x-hidden">
     <NuxtLayout>
 			<NuxtPage />
 		</NuxtLayout>
--- components/layout/Hero.vue
+++ components/layout/Hero.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-  <section class="text-white py-[5rem]">
-		<h1 class="text-[5rem]">How to...</h1>
+<!--  <section class="text-white py-[5rem]">-->
+  <section>
+		<Title1>How to...</Title1>
 <!--		<h2 class="text-[3rem] lg:max-w-[50%]">A site where you can (perhaps) find the solution to your problem</h2>-->
 	</section>
 </template>
