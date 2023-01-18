23f0553850579d86acd3928b09f69629e05a6f69
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-01-19 00:51:36 +0100

    Randomize topics order on home page

--- store/TopicsStore.ts
+++ store/TopicsStore.ts
@@ -14,7 +14,21 @@ interface ITopicsStore {
 
 export const TopicsStore = defineStore('topics', (): ITopicsStore => {
 
-  const topics = ref(Topics.map((topic: any) => {
+  const randomNumberBetween = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const shuffleArray = (arr: Array<any>) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
+  const topics = ref(shuffleArray(Topics).map((topic: any) => {
     return {
       ...topic,
       to: {
