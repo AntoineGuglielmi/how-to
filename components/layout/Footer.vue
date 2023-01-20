1ca9a8a25982981f2a0bf19bda627dcf62230373
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-01-20 15:52:28 +0100

    Create footer + add favicon

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,11 +1,29 @@
 <script lang="ts" setup>
-
+	const footerLinks = [
+		{
+			icon: 'mdi:github',
+			href: 'https://github.com/AntoineGuglielmi/how-to',
+		}
+	]
 </script>
 
 <template>
   <footer class="py-[3em] lg:py-[2em] border-t border-gray-100/10 text-[1.5em]">
 		<Container>
-			<a href="https://github.com/AntoineGuglielmi/how-to" target="_blank"><Icon name="mdi:github" /></a>
+			<List
+				:items="footerLinks"
+				itemsNick="link"
+			>
+				<template v-slot="{ link }">
+					<a
+						:href="link.href"
+						target="_blank"
+						class="flex items-center"
+					>
+						<Icon v-if="link.icon" :name="link.icon"/>
+					</a>
+				</template>
+			</List>
 		</Container>
 	</footer>
 </template>
